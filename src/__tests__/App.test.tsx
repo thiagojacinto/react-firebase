1c98c6a044608077f0b9f32fa3332071d1f38487
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-08-09 23:54:32 -0300

    [test] fixing regex searching by "hidden" footer

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { render } from "@testing-library/react";
+
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import "@testing-library/jest-dom/extend-expect";
+
 import App from "../App";
 
 test("Deve ser renderizado o nome do autor: Thiago Jacinto", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/thiago jacinto/i);
+  const linkElement = getByText(/thiago jacinto/gis);
   expect(linkElement).toBeInTheDocument();
 });
