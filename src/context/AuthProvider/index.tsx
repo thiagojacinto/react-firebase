82b360f49dc9720b35e542b971cea3d2af23c1e7
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-08-12 23:58:44 -0300

    [authentication] provider to handle auth with email

---
+++ src/context/AuthProvider/index.tsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect, createContext } from "react";
+import firebase from "../../firebase";
+import "firebase/auth";
+
+type ContextProps = {
+  usuarioLogado: firebase.User | null;
+  estaAutenticado: boolean;
+  setUsuarioLogado: any;
+  loading: boolean;
+};
+
+export const AuthUserContext = createContext<Partial<ContextProps>>({});
+
+export const AuthProvider: React.FC = ({ children }) => {
+  let [usuarioLogado, setUsuarioLogado] = useState(
+    null as firebase.User | null
+  );
+
+  let [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user: any) => {
+      setUsuarioLogado(user);
+      setLoading(false);
+    });
+  }, []);
+
+  return (
+    <AuthUserContext.Provider
+      value={{
+        usuarioLogado,
+        estaAutenticado: usuarioLogado !== null,
+        setUsuarioLogado,
+        loading,
+      }}
+    >
+      {children}
+    </AuthUserContext.Provider>
+  );
+};
+
+export default AuthProvider;
